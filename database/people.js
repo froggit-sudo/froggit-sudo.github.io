03999cb0fb48d0d0fb61ba90e859d1642bdbedac
froggit-sudo <77854179+froggit-sudo@users.noreply.github.com>
2021-07-29 22:44:04 -0700

    Update people.js

--- database/people.js
+++ database/people.js
@@ -3,7 +3,7 @@ people.once("value", getData, error);
 function appendChild(firstName) {
     var aTag = document.createElement("A");
     aTag.innerHTML = "Name: " + firstName
-    aTag.href = window.location.href + "?name=" + firstName;
+    aTag.href = "https://froggit-sudo.github.io/database/person" + "?name=" + firstName;
     document.getElementById("people").appendChild(aTag);
 }
 
